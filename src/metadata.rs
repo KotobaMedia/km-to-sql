70a67ed2243fac555ccb04750f7b789460ed2ae8
Keita Kobayashi <keita@kbys.me>
2025-03-25 17:16:01 +0900

    Skip serializing None

--- src/metadata.rs
+++ src/metadata.rs
@@ -4,12 +4,18 @@ use url::Url;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TableMetadata {
     pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub desc: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub source: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub source_url: Option<Url>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub license: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub license_url: Option<Url>,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub primary_key: Option<String>,
     pub columns: Vec<ColumnMetadata>,
 }
@@ -17,9 +23,12 @@ pub struct TableMetadata {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ColumnMetadata {
     pub name: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub desc: Option<String>,
     pub data_type: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub foreign_key: Option<ColumnForeignKeyDetails>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub enum_values: Option<Vec<ColumnEnumDetails>>,
 }
 
@@ -32,5 +41,6 @@ pub struct ColumnForeignKeyDetails {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ColumnEnumDetails {
     pub value: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub desc: Option<String>,
 }
